e16e3c3b3729557179c43021b6be95ee3d504fd3
Rattanachai Sriwilai <duk151@hotmail.com>
2021-07-17 03:27:46 +0200

    implementing employee panels

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListEmployeesComponent } from './employees/list-employees/list-employees.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    ListEmployeesComponent,
   ],
   imports: [
     BrowserModule
