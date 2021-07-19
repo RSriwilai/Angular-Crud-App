8203019fea44ec76e70f40b4a83848bb5b566a93
Rattanachai Sriwilai <duk151@hotmail.com>
2021-07-20 01:08:19 +0200

    Started with create employee form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,30 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ListEmployeesComponent } from './employees/list-employees/list-employees.component';
+import { CreateEmployeeComponent } from './employees/create-employee/create-employee.component';
+import { createComponent } from '@angular/compiler/src/core';
 
+const appRoute : Routes = [
+  { path: 'list', component:ListEmployeesComponent },
+  { path: 'create', component:CreateEmployeeComponent },
+  { path: '', redirectTo: '/list', pathMatch: 'full' }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     ListEmployeesComponent,
+    CreateEmployeeComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoute),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
